550fd9c2d70296d512c0fe982e17aa52137995a0
Victor Anaya <victorgotzilla@hotmail.com>
2020-10-28 21:48:23 -0600

    idk what exactly is this THING

--- threads_in_C/threads4.c
+++ threads_in_C/threads4.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <netdb.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <stdlib.h>//check
 #include <string.h>
 #include <stdint.h>
 //trhead safety
+
+//standard HTTP port
+#define SERVER_PORT 80
+
+#define MAXLINE 4096
+#define SA struct sockaddr
+
 
 #define BIG 1000000000UL
 uint32_t counter = 0;
@@ -17,6 +35,32 @@ void * count_to_big(void * arg){
         counter++;
         pthread_mutex_unlock(&lock); 
     }
+}
+
+char *getHomePage(char *ipaddr, int *bytesread){
+    int sockfd, n;
+    int sendbytes;
+    struct sockaddr_in servaddr;
+    char sendline[MAXLINE];
+    char *buffer = malloc(MAXLINE);
+
+    if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0){
+        return NULL;
+    }
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(SERVER_PORT);
+
+    if(inet_pton(AF_INET, ipaddr, &servaddr.sin_addr)<=0){
+        return NULL;
+    }
+
+    if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr))<0)
+    {
+        return NULL;
+    }
+    
+    
 }
 
 int main(){
