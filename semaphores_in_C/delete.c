7cf0afc271576f9bb53489b0e94e37dab70fcd12
Victor Anaya <victorgotzilla@hotmail.com>
2020-11-18 11:55:27 -0500

    2 critical sections, 2 producers, 2 consumers

--- semaphores_in_C/delete.c
+++ semaphores_in_C/delete.c
@@ -10,7 +10,7 @@ void * producer();
 void * consumer();
 
 //sm y sm2 is the critical section semaphore
-sem_t empty, full, sm,sm2;
+sem_t empty, full, sm,sm2,sm_cons, sm2_cons;
 
 //secciones criticas
 int data1, data2;
@@ -32,11 +32,13 @@ int main(){
     sem_init(&full,SHARED,0);
     sem_init(&sm,SHARED,1);
     sem_init(&sm2,SHARED,1);
+    sem_init(&sm_cons, SHARED, 0);
+    sem_init(&sm2_cons, SHARED, 0);
     
     pthread_create(&thread1, NULL, producer, (void*)a);
     pthread_create(&thread2, NULL, producer, (void*)b);
     //pthread_create(&thread_test, NULL, producer, (void*)c);
-    sleep(1);
+    //sleep(1);
     pthread_create(&cons1, NULL, consumer, (void*)a);
     pthread_create(&cons2, NULL, consumer, (void*)b);
 
@@ -51,24 +53,28 @@ int main(){
 
 //YA NOMAS FALTA PONER LOS PUTOS FOR Y YA ALV CHINGO ASUMADRE LA TAREAAA
 void * producer(void * no){
-    int produced=10;
+    int produced=0;
     int *thread = (int*)no;
-    printf("\nProducer %d Created",*thread);
-    printf("\nProducer id is %ld\n",pthread_self());
-    sem_wait(&empty);
-        preguntar(produced);
-    sem_post(&full);
+    printf("\nProducer %d Created---",*thread);
+    printf("Producer id is %ld\n",pthread_self());
+    for(produced=0;produced<3;produced++){
+        sem_wait(&empty);
+            preguntar(produced);
+        sem_post(&full);
+    }
     return NULL;
 }
 
 void * consumer(void * no){
     int consumed;
     int *thread = (int*)no;
-    printf("\nConsumer %d created",*thread);
-    printf("\nConsumer id is %ld\n",pthread_self());
-    sem_wait(&full);
-        preguntar_consumidor();
-    sem_post(&empty);
+    printf("\nConsumer %d created---",*thread);
+    printf("Consumer id is %ld\n",pthread_self());
+    for(consumed=0;consumed<3;consumed++){
+        sem_wait(&full);
+            preguntar_consumidor();
+        sem_post(&empty);
+    }
     return NULL;
 }
 
@@ -81,24 +87,24 @@ void preguntar(int num){
             //preguntar(numero);
         }else{
             printf("accediste a la seccion critica 2\n");
-            data2 = num+1;
+            data2 = num;
             printf("data2:%d\n",data2);
-            sem_post(&sm2);
+            sem_post(&sm2_cons);
             return NULL;
         }
     }else{
         printf("entraste en la seccion critica 1\n");
         data1 = num;
         printf("data1:%d\n",data1);
-        sem_post(&sm);
+        sem_post(&sm_cons);
         return NULL;
     }
 }
 
 void preguntar_consumidor(){
-    if (sem_trywait(&sm)==-1){
+    if (sem_trywait(&sm_cons)==-1){
         printf("no se puede Consumir lo que esta en la SC1, intentando consumir lo de la SC2\n");
-        if(sem_trywait(&sm2)==-1){
+        if(sem_trywait(&sm2_cons)==-1){
             printf("no se puede consumir lo de SC2, intentando consumir SC1\n");
             //preguntar_consumidor();
         }else{
