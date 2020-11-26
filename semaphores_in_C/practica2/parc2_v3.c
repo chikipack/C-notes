280a63c055b87117ced5ebbc3f58c6044be7d2a7
victor anaya <chiki@2806-1000-8004-d936-0000-0000-0000-0002.huawei.net>
2020-11-25 23:23:44 -0800

    practica 2 avanzando

--- semaphores_in_C/practica2/parc2_v3.c
+++ semaphores_in_C/practica2/parc2_v3.c
@@ -5,7 +5,7 @@
 #include <errno.h>
 #include <unistd.h>
 #define SHARED 1
-#define PRODUCCIONES 2
+#define PRODUCCIONES 3
 
 int value;
 
@@ -13,13 +13,14 @@ sem_t in_main_section, out_main_section;
 sem_t crit_sec[6];
 sem_t crit_sec_cons[6];
 
+FILE * fichero1;
+FILE * fichero2;
+
 int w=1,x=2,y=3,z=4;
 int *a=&w;
 int *b=&x;
 int *c=&y;
 int *d=&z;
-
-
 
 int data1, data2, data3, data4, data5, data6;
 
@@ -43,11 +44,15 @@ int main(){
     pthread_create(&prod2, NULL, producer,(void*)b);
     // pthread_create(&prod3, NULL, producer,(void*)c);
     // pthread_create(&prod4, NULL, producer,(void*)d);
+
+    pthread_create(&cons1,NULL,consumer,(void*)a);
     
     pthread_join(prod1,NULL);
     pthread_join(prod2,NULL);
     // pthread_join(prod3,NULL);
     // pthread_join(prod4,NULL);
+
+    pthread_join(cons1,NULL);
 
     
     return 0;
@@ -60,15 +65,17 @@ void * producer(void * no){
     int contador2222=1;
     int dato2=2222;
 
-    // int contador3333=1;
-    // int contador4444=1;
+    int contador3333=1;
+    int dato3=3333;
+    
+    int contador4444=1;
+    int dato4=4444;
 
     int *thread = (int*)no;
-    printf("Hi, i'm the producer %d -- ID: %ld\n",*thread,pthread_self());
+    printf("Hi, i'm the producer %d - ID: %ld\n",*thread,pthread_self());
     switch (*thread){
         case 1:
         printf("voy a producir 1111\n");
-        //vamos a la funcion producir
         producir(contador1111,dato1);
         break;
 
@@ -79,10 +86,12 @@ void * producer(void * no){
 
         case 3:
         printf("voy a producir 3333\n");
+        producir(contador3333,dato3);
         break;
         
         case 4:
         printf("voy a producir 4444\n");
+        producir(contador4444,dato4);
     }
 }
 
@@ -91,18 +100,49 @@ void producir(int contador, int dato){
     for(int i=0;i<PRODUCCIONES;i++){
         sem_wait(&in_main_section);
             preguntar(count, dato);
-            sem_getvalue(&in_main_section,&value);
-            printf("valor del semaforo: %d\n",value);
-            printf("produccion #%d\n",count);
+            //sem_getvalue(&in_main_section,&value);
+            //printf("valor del semaforo: %d\n",value);
+            //printf("produccion #%d\n",count);
             count++;
-            printf("dato: %d\n",dato);
+            // printf("dato: %d\n",dato);
         sem_post(&out_main_section);
     }
 
 }
 
-void consumir(){
+void * consumer(void * no){
+    int *thread = (int*)no;
+    printf("\nConsumer %d created-ID %d",*thread,pthread_self());
+    //este for nomas es para ver que jale 
+    //debemos de quitarlo y usar un while que pueda terminar de alguna forma
+    
+    for (int i = 0; i<6; i++){
+        consumir();
+    }
+    
 
+    // while(1){
+    //     /*
+    //     este ciclo va a estar activo 3 veces al mismo tiempo 
+    //     solo hay que estar revisando las 6 secciones criticas, en algun momneto algun hilo va a poder consumir algo 
+    //     en cada sc le ponemos la funcion y le madamos el dato
+    //     */
+    //    consumir();
+       
+    // }
+
+    // for(int i=0;i<PRODUCCIONES;i++){
+    //     sem_wait(&out_main_section);
+    //         preguntar_consumidor();
+    //     sem_post(&in_main_section);
+    // }
+}
+
+
+void consumir(){
+    sem_wait(&out_main_section);
+        preguntar_consumidor();
+    sem_post(&in_main_section);
 }
 
 
@@ -115,23 +155,23 @@ void crear_semaforos_CS(){
         sem_init(&crit_sec[i],SHARED,1);
     }
     int value;
-    for (int i = 0; i < 6; i++){
-        //sem_getvalue(&crit_sec[i],&value);
-        //printf("%d\n",value);
-    }
+    // for (int i = 0; i < 6; i++){
+    //     //sem_getvalue(&crit_sec[i],&value);
+    //     //printf("%d\n",value);
+    // }
     
     for(int i=0;i<6;i++){
         sem_init(&crit_sec_cons[i],SHARED,0);
     }
 
     int value2;
-    for (int i = 0; i < 6; i++){
-        //sem_getvalue(&crit_sec_cons[i],&value2);
-        //printf("%d\n",value2);
-    }
+    // for (int i = 0; i < 6; i++){
+    //     //sem_getvalue(&crit_sec_cons[i],&value2);
+    //     //printf("%d\n",value2);
+    // }
 }
 
-void preguntar(){
+void preguntar(int contador, int dato){
     if (sem_trywait(&crit_sec[0])==-1){
         printf("seccion critica 1 OCUPADA, intentando entrar a seccion critica 2\n");
         if (sem_trywait(&crit_sec[1])==-1){
@@ -144,24 +184,41 @@ void preguntar(){
                         printf("seccion critica 5 ocupada, intentando entrar a la seccion critica 6\n");
                         if(sem_trywait(&crit_sec[5])==-1){
                             printf("seccion critica 6 ocupada, intentando entrar a la seccion critica 1\n");
-                            preguntar();
+                            preguntar(contador, dato);
                         }else{
                             printf("Productor entro en la seccion critica 6\n");
+                            data6 = dato;
+                            sem_post(&crit_sec_cons[5]);
+                            return;
                         }
                     }else{
                         printf("Productor entro en la seccion critica 5\n");
+                        data5 = dato;
+                        sem_post(&crit_sec_cons[4]);
+                        return;
                     }
                 }else{
                     printf("Productor entro en la seccion critica 4\n");
+                    data4 = dato;
+                    sem_post(&crit_sec_cons[3]);
+                    return;
                 }
             }else{
                 printf("Productor entro en la seccion critica 3\n");
+                data3 = dato;
+                sem_post(&crit_sec_cons[2]);
+                return;
             }
         }else{
             printf("Productor entro a la seccion critica 2\n");
+            data2 = dato;
+            sem_post(&crit_sec_cons[1]);
+            return;
         }
     }else{
         printf("Productor entro en la seccion critica 1\n");
+        data1 = dato;
+        sem_post(&crit_sec_cons[0]);
         return;
     }
 }
@@ -182,20 +239,131 @@ void preguntar_consumidor(){
                             preguntar_consumidor();
                         }else{
                             printf("se consumio SC6\n");
+                            switch(data6){
+                                case 1111:
+                                printf("se guardo el dato 1111\n");
+                                fichero1 = fopen("unos.txt","w");
+                                fprintf(fichero1,"%d",data6);
+                                break;
+
+                               case 2222:
+                                printf("se guardo el dato 2222\n");
+                                fichero2 = fopen("doses.txt","w");
+                                fprintf(fichero2,"%d",data6);
+                                break;
+
+                                case 3333:
+                                printf("se guardo el dato 3333\n");
+                                break;
+
+                                case 4444:
+                                printf("se guardo el dato 4444\n");
+                                break;
+                            }
+                            //escribir_txt();
                         }
                     }else{
                         printf("se consumio SC5\n");
+                        switch(data5){
+                            case 1111:
+                            printf("se guardo el dato 1111\n");
+                            break;
+
+                            case 2222:
+                            printf("se guardo el dato 2222\n");
+                            break;
+
+                            case 3333:
+                            printf("se guardo el dato 3333\n");
+                            break;
+
+                            case 4444:
+                            printf("se guardo el dato 4444\n");
+                            break;
+                        }
                     }
                 }else{
                     printf("se consumio SC4\n");
+                    switch(data4){
+                        case 1111:
+                        printf("se guardo el dato 1111\n");
+                        break;
+
+                        case 2222:
+                        printf("se guardo el dato 2222\n");
+                        break;
+
+                        case 3333:
+                        printf("se guardo el dato 3333\n");
+                        break;
+
+                        case 4444:
+                        printf("se guardo el dato 4444\n");
+                        break;
+                        }
                 }
             }else{
                 printf("se consumio SC3\n");
+                switch(data3){
+                    case 1111:
+                    printf("se guardo el dato 1111\n");
+                    break;
+
+                    case 2222:
+                    printf("se guardo el dato 2222\n");
+                    break;
+
+                    case 3333:
+                    printf("se guardo el dato 3333\n");
+                    break;
+
+                    case 4444:
+                    printf("se guardo el dato 4444\n");
+                    break;                    
+                }
             }
         }else{
             printf("se consumio SC2\n");
+            switch(data2){
+                case 1111:
+                printf("se guardo el dato 1111\n");
+                break;
+
+                case 2222:
+                printf("se guardo el dato 2222\n");
+                break;
+
+                case 3333:
+                printf("se guardo el dato 3333\n");
+                break;
+
+                case 4444:
+                printf("se guardo el dato 4444\n");
+                break;                    
+            }
         }
     }else{
         printf("se consumio SC1\n");
+        switch(data1){
+            case 1111:
+            printf("se guardo el dato 1111\n");
+            break;
+
+            case 2222:
+            printf("se guardo el dato 2222\n");
+            break;
+
+            case 3333:
+            printf("se guardo el dato 3333\n");
+            break;
+
+            case 4444:
+            printf("se guardo el dato 4444\n");
+            break;                    
+        }
     }   
+}
+
+void escribir_txt(){
+
 }
